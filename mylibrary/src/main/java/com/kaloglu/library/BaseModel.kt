9825bb337484a194d53367ae49209f94c0207447
kaloglu <cihan@kaloglu.com>
2020-02-02 17:13:36 +0300

    fix BaseModel

--- mylibrary/src/main/java/com/kaloglu/library/BaseModel.kt
+++ mylibrary/src/main/java/com/kaloglu/library/BaseModel.kt
@@ -2,12 +2,7 @@ package com.kaloglu.library
 
 import java.io.Serializable
 
-abstract class BaseModel : Serializable {
-    open var id: String = String.empty
-
-    companion object {
-        const val serialVersionUID = 1L
-    }
-
+interface BaseModel : Serializable {
+    var id: Long
 }
 
